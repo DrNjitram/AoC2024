13f3e59eca795d5d93da2316ec0acbde751b4d6f
DrNjitram <martijn.gobes@gmail.com>
2024-12-02 11:07:06 +0100

    Day 2

---
+++ Day2.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from util import read_lines
+
+def safety(report):
+    grad = np.ediff1d(report)
+    return np.all(np.abs(grad)>0) and np.all(np.abs(grad)<4) and (np.all(grad<0) or np.all(grad>0))
+
+answer = 0
+answer2 = 0
+for line in read_lines("Inputs/Day2"):
+    report = [int(i) for i in line.split()]
+    safe = safety(report)
+    answer += safe
+    answer2 += safe
+    if not safe:
+        for i in range(len(report)):
+            rep2 = report[:]
+            rep2.pop(i)
+            if safety(rep2):
+                answer2+=1
+                break
+
+print(answer)
+print(answer2)
+
