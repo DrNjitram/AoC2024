eab915b2592df60614547ab5f81e05a75984d474
DrNjitram <martijn.gobes@gmail.com>
2024-12-11 22:01:45 +0100

    Day 11 - 1 second short

--- Day11.py
+++ Day11.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from operator import add
 from util import *
 
 memory = {}
@@ -10,26 +12,26 @@ def step_stone(stone):
     else:
         return [stone*2024]
 
+def get_num_stones(stones, step):
+    if not step:
+        return len(stones)
+    length = 0
+    for stone in stones:
+        if (stone, step) in memory:
+            ans = memory[(stone, step)]
+        else:
+            ans = get_num_stones(step_stone(stone), step - 1)
+            memory[(stone, step)] = ans
+        length += ans
+    return length
+
 def part1(Lines):
-    answer = 0
     stones = Lines[0]
-    while len(stones)>0:
-        stone = stones.pop(0)
-        for step in range(2):
-            print(stone)
-            if (stone, step) in memory:
-                print("found stone", memory[stone])
-            else:
-                next_stones = step_stone(stone)
-                stone = next_stones.pop(0)
-                if len(next_stones)>0:
-                    stones.append(next_stones.pop(0))
-                #memory[(stone, step)] = step_stone(stone)
-
-                print(f"{stone} -> {step_stone(stone)}")
-
-    print(answer)
-    return answer
+    answer1 = get_num_stones(stones, 25)
+    answer2 = get_num_stones(stones, 75)
+    print(answer1, answer2)
+    return answer1, answer2
 
 if __name__ == '__main__':
-    test(read_day(11, 1, split=True, cast=int), part1, 55312)
+    test(read_day(11, 1, split=True, cast=int), part1, (55312, 65601038650482))
+    test(read_day(11, split=True, cast=int), part1, (216996, 257335372288947))
