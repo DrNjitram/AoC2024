ec9edc81c2ccc6434ad72d15c3e57c294f9527ad
lolface1008 <martijn.gobes@gmail.com>
2024-12-19 22:19:38 +0800

    Day 19 - Imagine writing your own memoization

--- Day19.py
+++ Day19.py
@@ -1,32 +1,25 @@
+from functools import cache
 from util import *
 
-data = {}
+towels = []
 
-def find_combos(pattern, towels):
+@cache
+def find_combos(pattern):
     if not pattern:
         return 1
     poss = 0
     for towel in towels:
         if pattern.startswith(towel):
-            next_pattern = pattern[len(towel):]
-
-            if next_pattern not in data:
-                data[next_pattern] = find_combos(pattern[len(towel):], towels)
-            poss += data[next_pattern]
-
-    data[pattern] = poss
+            poss += find_combos(pattern[len(towel):])
     return poss
 
 def parts(lines):
-    global data
-    data = {}
+    global towels
+    find_combos.cache_clear()
     towels = lines[0].split(", ")
-    combinations = []
-    for pattern in lines[2:]:
-        combinations.append(find_combos(pattern, towels))
+    combinations = [find_combos(pattern) for pattern in lines[2:]]
 
     print(len([c for c in combinations if c]), sum(combinations))
-
     return len([c for c in combinations if c]), sum(combinations)
 
 if __name__ == '__main__':
